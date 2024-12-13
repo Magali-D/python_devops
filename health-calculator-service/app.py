eab62326905bd0e55085a1842e262e135a62d30f
Magali-D <magali.paoli@tutanota.com>
2024-12-13 16:13:20 +0100

    Corrected app so that container exposed

--- health-calculator-service/app.py
+++ health-calculator-service/app.py
@@ -28,4 +28,4 @@ def subtract_numbers():
         return jsonify({"error": "Missing required parameters 'height', 'weight', 'age' or 'gender'"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
